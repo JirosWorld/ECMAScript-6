02c89a187065a982b4c2e434014e6cf953b9ba03
Jirosworld <jolarti@gmail.com>
2022-08-23 15:18:22 +0200

    use primitive Typescript in calculator - start

--- typescript/index.js
+++ typescript/index.js
@@ -1,40 +1,22 @@
-// JavaScript Types....
-// Primitives:
-// Boolean : typeof instance === "boolean"
-// Number : typeof instance === "number"
-// String : typeof instance === "string"
-// BigInt : typeof instance === "bigint" = beyond safe numbers
-// Symbol : typeof instance === "symbol" = 'atoms'
-// undefined : typeof instance === "undefined" = error outcome of code
-
-
-// Structural Types:
-// Object : typeof instance === "object"
-// Function : typeof instance === "function"
-
-// Structural Root Primitive:
-// null : typeof instance === "object" = an emptiness defined by the programmer
-
-// Using typeof
-
-const button = document.querySelector(".button");
+"use strict";
+// Benefits of TypeScript
+// challenges:
+// - give the screen's innerHTML a string
+// - change the two input values to numbers
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.screen = void 0;
+const button = document.querySelector('.button');
+// tell Typescript the input is not a variable but an HTML element
 const firstInput = document.querySelector('#first-input');
-const secondInput = document.querySelector('#second-input');
-const screen = document.querySelector('.screen');
-
-// this gives a problem: it turns the inputs into strings
-// function addNumbers(a, b) {
-//     console.log(a + b);
-//     screen.innerHTML = a + b;
-// }
-
-// easy solution: check logic/types after data is input
-function addNumbers(a,b) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        screen.innerHTML = a + b
-    } else screen.innerHTML = parseInt(a) + parseInt(b)
+const secondInput = document.querySelector('#secnd-input');
+// make element export becasue it will be changed in the function below
+exports.screen = document.querySelector('.screen');
+// add types to parameters
+function addNumbers(a, b) {
+    //reassign html field to new string
+    exports.screen.innerHTML = (a + b).toString();
 }
-
-// note: in Typescriot we put the logic INSIDE the parameters, BEFORE input
-
-button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value));
+button.addEventListener('click', () => 
+// fix strings into numbers
+addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)));
+//# sourceMappingURL=index.js.map
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,3 +1,21 @@
-let age: number = 34
-let firstName: string = 'Angela'
-let busy: boolean = true
+// Benefits of TypeScript
+// challenges:
+// - give the screen's innerHTML a string
+// - change the two input values to numbers
+
+const button = document.querySelector('.button')
+// tell Typescript the input is not a variable but an HTML element
+const firstInput = document.querySelector('#first-input') as HTMLInputElement
+const secondInput = document.querySelector('#secnd-input') as HTMLInputElement
+// make element export becasue it will be changed in the function below
+export const screen = document.querySelector('.screen')
+ 
+// add types to parameters
+function addNumbers(a: number,b: number) {
+    //reassign html field to new string
+     screen.innerHTML = (a + b).toString()
+}
+ 
+button.addEventListener('click', () => 
+// fix strings into numbers
+    addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
