f55ba513984540c1a1ab13e79ee6feccb4314e26
Jirosworld <jolarti@gmail.com>
2022-08-23 19:53:10 +0200

    add properties of a location (needs webpack)

---
+++ typescript/airbnb-project/utils.ts
@@ -0,0 +1,17 @@
+// move constants here if they are needed for the functions
+const reviewTotalDisplay = document.querySelector('#reviews')
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+// both functions are separately exported
+export function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning == true){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
